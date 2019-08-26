713c1039ecfbeeaa848bb8b983082dd5ab87e9b9
Satore13 <53190848+Satore13@users.noreply.github.com>
2019-08-26 14:12:09 +0200

    Added error managing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+#[macro_use]
+extern crate error_chain;
+
 use std::fs::File;
 use std::io::Read;
 use argparse::{ArgumentParser, Store};
-mod chip8;
-use chip8::Chip8;
 
-fn load_rom(rom_name: String) -> Result<[u8; 0xE00], std::io::Error>
+mod chip8;
+use chip8::{Chip8, Result, ResultExt};
+
+fn load_rom(rom_name: String) -> Result<[u8; 0xE00]>
 {
     let mut rom = File::open(&rom_name)?;
     let mut brom: Vec<u8> = Vec::new();
@@ -12,9 +16,8 @@ fn load_rom(rom_name: String) -> Result<[u8; 0xE00], std::io::Error>
     let mut f_rom = [0u8; 0xE00];
     if brom.len() > f_rom.len()
     {
-        return Err(std::io::Error::new(std::io::ErrorKind::InvalidData,
-                                format!("Rom can't be bigger than {}B and {} is {}B",
-                                        f_rom.len(), rom_name , brom.len())));
+        bail!(format!("Rom can't be bigger than {}B and {} is {}B",
+                                        f_rom.len(), rom_name , brom.len()));
     }
     (&mut f_rom[..brom.len()]).copy_from_slice(&brom[..]);
     Ok(f_rom)
@@ -22,6 +25,26 @@ fn load_rom(rom_name: String) -> Result<[u8; 0xE00], std::io::Error>
 
 
 fn main()
+{
+    if let Err(e) = run()
+    {
+        println!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace()
+        {
+            println!("backtrace: {:?}", backtrace);
+        }
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()>
 {
     let mut rom_name = String::new();
     {
@@ -34,14 +57,8 @@ fn main()
                 ap.parse_args_or_exit();
     }
 
-
-    match load_rom(rom_name)
-    {
-        Ok(rom) =>
-        {
-            let mut chip = Chip8::new(Some(rom));
-            chip.auto_run();
-        }
-        Err(e) => eprintln!("Error reading the rom: \n{}",e),
-    }
+    let rom = load_rom(rom_name).chain_err(|| "Error loading rom")?;
+    let mut chip = Chip8::new(Some(rom)).chain_err(|| "Error creating Chip8 struct")?;
+    chip.auto_run().chain_err(|| "Error executing rom")?;
+    Ok(())
 }
